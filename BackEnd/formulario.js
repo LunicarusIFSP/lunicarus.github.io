c5fd052cadf6c4e7d8dcec42b501cad119bc1105
lucas <lucas6jau@gmail.com>
2022-06-17 17:53:00 -0300

    a

--- BackEnd/formulario.js
+++ BackEnd/formulario.js
@@ -1,8 +1,11 @@
 function formulario(){
     var client = {
-        client_name : document.getElementById("name"),
-        client_tel : document.getElementById("tel"),
-        client_email
-    }
-    alert("");
+        client_name : "Anonymous",
+        client_tel : 000000,
+        client_email : "usermail@hotmail.com"
+    };
+    client.client_name = document.getElementById("name").value;
+    client.client_tel = document.getElementById("tel").value;
+    client.client_email = document.getElementById("email").value;
+    document.getElementById("confirmation").innerHTML = "Confirmation: hello '" + client.client_name + "' an email was sent to the adress: '" + client.client_email + "' and an WhatsApp BOT will also contact you at: " + client.client_tel;
 }
