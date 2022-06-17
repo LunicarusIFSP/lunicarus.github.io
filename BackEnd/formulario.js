35638e9c4613eb843f42a3ad18442e0b40b89169
lucas <lucas6jau@gmail.com>
2022-06-17 17:27:38 -0300

    works

---
+++ BackEnd/formulario.js
@@ -0,0 +1,8 @@
+function formulario(){
+    var client = {
+        client_name : document.getElementById("name"),
+        client_tel : document.getElementById("tel"),
+        client_email
+    }
+    alert("");
+}
